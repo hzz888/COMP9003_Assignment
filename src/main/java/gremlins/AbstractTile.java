1b6e7dfc91e9043f6558f372e5c049b2866020ee
hzz <60465723+hzz888@users.noreply.github.com>
2022-09-29 22:10:39 +0800

    progress

--- src/main/java/gremlins/AbstractTile.java
+++ src/main/java/gremlins/AbstractTile.java
@@ -5,8 +5,11 @@ import processing.core.PImage;
 /**
  * @author hzz
  */
-abstract  public class AbstractTile {
-  private static final int WIDTH = 20;
-  private static final int HEIGHT = 20;
-  private PImage tileSprite;
+abstract public class AbstractTile {
+    private static final int WIDTH = 20;
+    private static final int HEIGHT = 20;
+    private PImage tileSprite;
+    private int x;
+    private int y;
+
 }
--- src/main/java/gremlins/App.java
+++ src/main/java/gremlins/App.java
@@ -1,5 +1,7 @@
 package gremlins;
 
+import java.util.Objects;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.data.JSONObject;
@@ -7,6 +9,7 @@ import processing.data.JSONArray;
 
 import java.util.Random;
 import java.io.*;
+import java.util.Scanner;
 
 
 /**
@@ -18,15 +21,18 @@ public class App extends PApplet {
     public static final int HEIGHT = 720;
     public static final int SPRITESIZE = 20;
     public static final int BOTTOMBAR = 60;
-
     public static final int FPS = 60;
-
     public static final Random RANDOM_GENERATOR = new Random();
 
+
     public String configPath;
-    
     public PImage brickwall;
     public PImage stonewall;
+    public PImage exit;
+    public PImage gremlin;
+    public PImage wizard;
+    public PImage slime;
+    public PImage fireball;
 
     public App() {
         this.configPath = "config.json";
@@ -34,57 +40,82 @@ public class App extends PApplet {
 
     /**
      * Initialise the setting of the window size.
-    */
+     */
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
     }
 
     /**
-     * Load all resources such as images. Initialise the elements such as the player, enemies and map elements.
-    */
+     * Load all resources such as images. Initialise the elements such as the player, enemies and map
+     * elements.
+     */
     @Override
     public void setup() {
+
         frameRate(FPS);
+        surface.setTitle("Gremlins-by hzz");
+        surface.setResizable(false);
 
         // Load images during setup
-        this.stonewall = loadImage(this.getClass().getResource("stonewall.png").getPath().replace("%20", ""));
-        this.brickwall = loadImage(this.getClass().getResource("brickwall.png").getPath().replace("%20", ""));
-        //this.gremlin = loadImage(this.getClass().getResource("gremlin.png").getPath().replace("%20", ""));
-        //this.slime = loadImage(this.getClass().getResource("slime.png").getPath().replace("%20", ""));
-        //this.fireball = loadImage(this.getClass().getResource("fireball.png").getPath().replace("%20", ""));
-        
+        this.stonewall = loadImage(
+                Objects.requireNonNull(this.getClass().getResource("stonewall.png"))
+                        .getPath().replace("%20", " "));
+        this.brickwall = loadImage(
+                Objects.requireNonNull(this.getClass().getResource("brickwall.png"))
+                        .getPath().replace("%20", " "));
+        this.gremlin = loadImage(
+                Objects.requireNonNull(this.getClass().getResource("gremlin.png")).getPath()
+                        .replace("%20", " "));
+        this.slime = loadImage(Objects.requireNonNull(this.getClass().getResource("slime.png")).getPath().replace("%20", " "));
+        this.fireball = loadImage(
+                Objects.requireNonNull(this.getClass().getResource("fireball.png")).getPath().replace("%20", " "));
 
         JSONObject conf = loadJSONObject(new File(this.configPath));
+        int level = 0;
+        String layOutPath = "../../" + conf.getJSONArray("levels").getJSONObject(level).getString("layout");
+        File layOutFile = new File(Objects.requireNonNull(this.getClass().getResource(layOutPath)).getFile());
+        try {
+            Scanner layOutScanner = new Scanner(layOutFile);
+            while (layOutScanner.hasNextLine()){
+                char[] line = layOutScanner.nextLine().toCharArray();
+                for(char c : line){
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
 
 
     }
 
     /**
      * Receive key pressed signal from the keyboard.
-    */
+     */
     @Override
-    public void keyPressed(){
+    public void keyPressed() {
 
     }
-    
+
     /**
      * Receive key released signal from the keyboard.
-    */
+     */
 
     @Override
-    public void keyReleased(){
+    public void keyReleased() {
 
     }
 
 
     /**
-     * Draw all elements in the game by current frame. 
-	 */
+     * Draw all elements in the game by current frame.
+     */
 
     @Override
     public void draw() {
-        background(197,151,113);
+        background(197, 151, 113);
         textSize(40);
 
     }
