e7c29699bc345dc6489a97a46ab2f8e0cdbc91ba
hzz <60465723+hzz888@users.noreply.github.com>
2022-10-12 23:57:06 +0800

    add

--- src/main/java/gremlins/Gremlin.java
+++ src/main/java/gremlins/Gremlin.java
@@ -6,14 +6,42 @@ package gremlins;
 public class Gremlin extends AbstractObject {
 
     public int gremlinMoveSpeed;
+    public String direction;
+    protected String[] gremlinDirections;
 
     public Gremlin(App app, int x, int y) {
         super(app, app.gremlinImage, x, y);
+        this.gremlinMoveSpeed = 1;
+        this.gremlinDirections = new String[]{"up", "down", "left", "right"};
+        this.direction = this.gremlinDirections[(int)(Math.random() * this.gremlinDirections.length)];
     }
+
+
+
     @Override
     public void tick(App app){
+        switch (this.direction){
+            case "left" :
+                this.x -= this.gremlinMoveSpeed;
+                break;
+            case "right" :
+                this.x += this.gremlinMoveSpeed;
+                break;
+            case "up" :
+                this.y -= this.gremlinMoveSpeed;
+                break;
+            case "down" :
+                this.y += this.gremlinMoveSpeed;
+                break;
+            default:
+                break;
+        }
+        this.wallCollision(app);
         this.draw(app);
-        return;
+    }
+
+    private void wallCollision(App app) {
+
     }
 }
 
