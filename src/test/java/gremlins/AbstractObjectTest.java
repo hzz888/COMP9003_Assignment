712b381bd0f58f8941c928cefa7a3b3465696f47
hzz <60465723+hzz888@users.noreply.github.com>
2022-10-17 18:22:25 +0800

    change jdk

--- src/test/java/gremlins/AbstractObjectTest.java
+++ src/test/java/gremlins/AbstractObjectTest.java
@@ -3,21 +3,33 @@ package gremlins;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import processing.core.PImage;
+import org.junit.jupiter.api.Test;
 
+import processing.core.PApplet;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AbstractObjectTest {
-
+    App app;
+    TestObject testObject;
     @BeforeEach
     void setUp() {
+        app = new App();
+        PApplet.runSketch(new String[]{"App"}, app);
+        app.setup();
+        app.gameStarted = true;
+        app.loop();
+        testObject = new TestObject(app,app.stoneWallImage,0,0);
     }
 
     @AfterEach
     void tearDown() {
+        app = null;
     }
 
     @Test
     void drawObject() {
+
     }
 
     @Test
@@ -42,5 +54,16 @@ class AbstractObjectTest {
 
     @Test
     void collide() {
+    }
+}
+
+class TestObject extends AbstractObject{
+    public TestObject(App app, PImage image, int x, int y){
+        super(app,image,x,y);
+    }
+
+    @Override
+    public void tick(App app){
+        app.image(this.objectSprite,x,y);
     }
 }
