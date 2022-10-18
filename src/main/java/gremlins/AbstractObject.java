de29e67cd16e693ef592b30fd5cbe43b3fefda74
hzz <60465723+hzz888@users.noreply.github.com>
2022-10-18 10:31:58 +0800

    Update AbstractObject.java

--- src/main/java/gremlins/AbstractObject.java
+++ src/main/java/gremlins/AbstractObject.java
@@ -5,50 +5,77 @@ import processing.core.PImage;
 
 
 /**
+ * AbstractObject is the base class for all objects in the game.
  * @author hzz
  */
 abstract public class AbstractObject {
+
     public final int WIDTH = 20;
     public final int HEIGHT = 20;
     protected PImage objectSprite;
     public int x;
     public int y;
 
+    /**
+     * The AbstractObject class is the parent class for all objects in the game.
+     * @param app The main App instance to run the game.
+     * @param objectSprite The sprite of the object.
+     * @param x The x coordinate of the object.
+     * @param y The y coordinate of the object.
+     */
     public AbstractObject(App app, PImage objectSprite, int x, int y) {
         this.objectSprite = objectSprite;
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * The draw method draws the object.
+     * @param app The main App instance to run the game.
+     */
     public void drawObject(App app) {
         app.image(this.objectSprite, this.x, this.y);
     }
 
+    /**
+     * Get the x coordinate of the object.
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * Get the y coordinate of the object.
+     */
     public int getY() {
         return this.y;
     }
 
+    /**
+     * Set the x coordinate of the object.
+     * @param x The new x coordinate of the object.
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Set the y coordinate of the object.
+     * @param y The new y coordinate of the object.
+     */
     public void setY(int y) {
         this.y = y;
     }
 
     /**
-     * Defines objects behaviors.
+     * Defines objects behaviors in each frame.
      * @param app The main app.
      */
     protected abstract void tick(App app);
 
     /**
      * Detect whether this object collides with another object.
-     *
+     * @param object Another object.
      * @return object if it collides with this instance
      */
     public AbstractObject collide(AbstractObject object) {
