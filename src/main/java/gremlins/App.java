ba49b26d7766033692e1a9d6cf2284cb9335ad4b
hzz <60465723+hzz888@users.noreply.github.com>
2022-10-20 13:17:53 +0800

    Update App.java

--- src/main/java/gremlins/App.java
+++ src/main/java/gremlins/App.java
@@ -29,8 +29,6 @@ public class App extends PApplet {
 
     public int x = 0;
     public int y = 0;
-    public int playerStartX;
-    public int playerStartY;
     public String configPath;
     public String layOutName = "";
 
@@ -379,8 +377,6 @@ public class App extends PApplet {
                             break;
                         case 'W':
                             this.player = new Wizard(this, this.x, this.y);
-                            this.playerStartX = this.x;
-                            this.playerStartY = this.y;
                             map[i][j] = null;
                             break;
                         case 'B':
